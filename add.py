5e5cb3cd349dd44488a5bb168ba27547b272ebfe
Ilyas Hiyane <ilyashiyane2004@gmail.com>
2026-03-18 17:57:34 +0000

    Rich

--- add.py
+++ add.py
@@ -1,8 +1,12 @@
 import json
 from datetime import datetime
 from dotenv import load_dotenv
+from rich.console import Console
+from rich.panel import Panel
+from rich.align import Align
 import os
 def Add(name) :
+     console=Console()
      load_dotenv()
      diaries_path = os.getenv("DIARIES_PATH")
      try:
@@ -14,7 +18,7 @@ def Add(name) :
      dats = input("Please provide a date in this format: year-month-day (YYYY-MM-DD). ")
      hour = input("Please provide the current time as Hour:Minute (HH:MM). ")
      if not dats.strip() or not hour.strip():
-            print("Enter both date and time. Leaving either blank will default to the current date and time.")
+            console.print("Enter both date and time. [bold red]Leaving either blank will default to the current date and time.")
             i = input("Press 1 for the current date and time, or 0 to enter your own date and hour. ")
             if i == "1":
                         dates = datetime.now()
